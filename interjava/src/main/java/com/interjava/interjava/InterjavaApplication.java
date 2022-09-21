74562daac426df3e63bdc6cf986764ed26a87c48
Zorrox <mecatr_onics@hotmail.com>
2022-09-21 08:46:16 -0500

    Se sube primer commit
    Se sube primer commit

---
+++ interjava/src/main/java/com/interjava/interjava/InterjavaApplication.java
@@ -0,0 +1,13 @@
+package com.interjava.interjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterjavaApplication.class, args);
+	}
+
+}
---
+++ interjava/src/test/java/com/interjava/interjava/InterjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interjava.interjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
